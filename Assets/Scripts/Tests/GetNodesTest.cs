5ff168757bf34e1f40a7416a9d363ffdc02ced6b
ddlibaneo <daniel.libaneo@gmail.com>
2018-05-13 12:05:38 -0300

    test folder and initial commit

---
+++ Assets/Scripts/Tests/GetNodesTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetNodesTest : MonoBehaviour {
+	public List<Planet> planetList = new List<Planet>();
+	public List<Explanation> paragraphs = new List<Explanation>();
+	void Awake () {
+
+	}
+
+	public void GetPlanetNodes() {
+		planetList.Clear();
+		
+		DatabaseManager.sharedInstance.GetPlanets(result => {
+			planetList = result;	
+			foreach (var item in planetList)
+			{
+				Debug.Log(item.planet);
+			}
+		});
+		
+	}
+
+	public void GetExplanations() {
+		paragraphs.Clear();
+		
+		DatabaseManager.sharedInstance.GetExplanation(result => {			
+			paragraphs = result;
+			foreach (var item in paragraphs)
+			{
+				Debug.Log(item.explanation);
+			}
+		});
+		
+	}
+}
