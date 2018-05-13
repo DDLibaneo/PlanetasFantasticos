a4dc33dad8e1698e869c023301f2d0f404d892a9
ddlibaneo <daniel.libaneo@gmail.com>
2018-05-13 17:50:08 -0300

    Add model NodePath

---
+++ Assets/Scripts/Model/NodePath.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class NodePath {
+	
+	public string Planet { get; set; }
+	public string Theme { get; set; }
+	public string Subject { get; set; }
+
+	public NodePath() {
+
+	}
+
+	public NodePath (string Planet, string Theme, string Subject) {
+		this.Planet = Planet;
+		this.Theme = Theme;
+		this.Subject = Subject;
+	}
+
+	public NodePath (string Planet, string Theme) {
+		this.Planet = Planet;
+		this.Theme = Theme;
+	}
+
+	public NodePath (string Planet) {
+		this.Planet = Planet;
+	}
+}
