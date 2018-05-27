74067ea1afc7f51998d0aec2fc81f6ea17328e5b
ddlibaneo <daniel.libaneo@gmail.com>
2018-05-27 19:22:23 -0300

    add Utilities scripts (stringManager, Etags)

---
+++ Assets/Scripts/Utilities/ETags.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ETags {
+
+	public static string tagAlternativas = "Alternativas";
+	
+	public static string tagTitlePlanet = "TitlePlanet";
+	public static string tagTitleSubject = "TitleSubject";
+	
+	public static string tagToggleQuestao = "ToggleQuestao";
+	public static string tagToggleTeoria = "ToggleTeoria";
+	public static string tagToggleResposta = "ToggleResposta";
+
+	public static string tagTotalQuestions = "TotalQuestions";
+	public static string tagTotalParagraphs = "TotalParagraphs";
+
+	public static string tagContentTeoria = "ContentTeoria";
+	public static string tagContentQuestao = "ContentQuestao";
+}
